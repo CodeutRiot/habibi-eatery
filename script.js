3b6bc6d38426fbf59d45eba9d8f1a5081cfbdc6b
Bryan Cayabyab <101242011+CodeutRiot@users.noreply.github.com>
2025-08-07 20:20:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This file is ready for your future JavaScript code!
+// For example, you could add a "back to top" button
+// or create an interactive photo gallery.
+
+console.log("Habibi Eatery's website script is loaded and ready!");
